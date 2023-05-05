8b3f9f348e723f60fda933b89fb9758c1fe16f12
Артур <s862008@yandex.ru>
2023-05-05 16:05:07 +0300

    Update bubble.go

--- sorting/bubble.go
+++ sorting/bubble.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-
+  
+  arr := []int{45,55,23,4,56,90,22}
+  
+  fmt.Println("исходный массив:",arr)
+  
+  n:=len(arr)
+  for i:=0;i<n-1;i++{
+	  for j:=0;j<n-i-1;j++{
+		  if arr[j]>arr[j+1]{
+			  arr[j],arr[j+1] = arr[j+1],arr[j]
+		  }
+	  }
+  }
+  
+fmt.Println("отсортированный массив:",arr)
+  
 }
