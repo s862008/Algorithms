d8656cd5559dc0a10d4b247f478384afd64ecd82
Артур <s862008@yandex.ru>
2023-12-07 11:55:43 +0300

    Update levenshtein_distance.go
    edit

--- others/levenshtein_distance.go
+++ others/levenshtein_distance.go
@@ -1,30 +1,37 @@
+//This code calculates the Levenshtein distance between two strings, s1 and s2. 
+//The Levenshtein distance is a measure of the minimum number of operations (insertions, deletions, or substitutions) needed to change one string into the other. 
+
 //Вот пример реализации алгоритма Левенштейна на языке Go:
 
 package main
 
-import "fmt"
+import (
+  "fmt"
+)
 
 func min(a, b, c int) int {
   if a < b && a < c {
     return a
-    } else if b < c {
+  } else if b < c {
     return b
-    } else {
+  } else {
     return c
-    }
+  }
 }
 
 func levenshteinDistance(s1, s2 string) int {
   m := len(s1)
   n := len(s2)
 
-// Создаем матрицу размером (m+1)x(n+1)
+  // Создаем матрицу размером (m+1)x(n+1)
+  // Initialize the dynamic programming table (DP table)
   d := make([][]int, m+1)
   for i := 0; i <= m; i++ {
     d[i] = make([]int, n+1)
   }
 
-// Инициализируем первую строку и столбец матрицы
+  // Инициализируем первую строку и столбец матрицы
+  // Initialize the first row and column of the DP table
   for i := 0; i <= m; i++ {
     d[i][0] = i
   }
@@ -32,18 +39,20 @@ func levenshteinDistance(s1, s2 string) int {
     d[0][j] = j
   }
 
-// Заполняем матрицу по правилу алгоритма
+  // Заполняем матрицу по правилу алгоритма
+  // Fill in the DP table
   for j := 1; j <= n; j++ {
     for i := 1; i <= m; i++ {
       cost := 0
       if s1[i-1] != s2[j-1] {
-      cost = 1
+        cost = 1
       }
-    d[i][j] = min(d[i-1][j]+1, d[i][j-1]+1, d[i-1][j-1]+cost)
+      d[i][j] = min(d[i-1][j]+1, d[i][j-1]+1, d[i-1][j-1]+cost)
     }
-  }  
+  } 
 
-// Возвращаем нижний правый элемент матрицы, содержащий расстояние Левенштейна
+  // Возвращаем нижний правый элемент матрицы, содержащий расстояние Левенштейна
+  // Calculate the minimum distance
   return d[m][n]
 }
 
@@ -51,8 +60,9 @@ func main() {
   s1 := "kitten"
   s2 := "sitting"
   distance := levenshteinDistance(s1, s2)
-  fmt.Printf("Расстояние Левенштейна между \"%s\" и \"%s\" равно %d\n", s1, s2, distance)
+  fmt.Printf("The Levenshtein distance between \"%s\" and \"%s\" is %d.\n", s1, s2, distance)
 }
+
 
 /*
 Этот код представляет собой функцию levenshteinDistance, которая принимает две строки s1 и s2 в качестве аргументов и возвращает расстояние Левенштейна между ними.
